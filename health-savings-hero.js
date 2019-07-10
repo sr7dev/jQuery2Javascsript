7ac12d9a0166a265e1ee415303e7b4495b04bbda
calmcalmdown <44122665+calmcalmdown@users.noreply.github.com>
2019-07-10 23:21:54 +0200

    Update health-savings-hero.js

--- health-savings-hero.js
+++ health-savings-hero.js
@@ -2,8 +2,5 @@
 /*HideNavandFooter of Default Template*/
 var image = document.querySelector("#page-id-474402 img#logo");
 
-if ( image )
-{
-	image.src = "/wp-content/uploads/2019/02/Health-Insurance-Quotes-logo.svg";
-}
+
 /*HideNavandFooter of Default Template*/
